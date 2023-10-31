4d4962568e788e59b370925d10dfdd1552f3134e
Luís Gustavo <luisgsilva270@gmail.com>
2023-10-31 00:29:18 +0000

    modulo-02/10-aula-tipo-boolean/booleanType.ts:

---
+++ modulo-02/10-aula-tipo-boolean/booleanType.js
@@ -0,0 +1,12 @@
+"use strict";
+let tarefaConcluida = true;
+let tarefaPendente = false;
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+const concluido = false;
+if (concluido) {
+    console.log('Tarefa foi concluída com sucesso!');
+}
+else {
+    console.log('Tarefa pendente!');
+}
---
+++ modulo-02/10-aula-tipo-boolean/booleanType.ts
@@ -0,0 +1,13 @@
+let tarefaConcluida: boolean = true
+let tarefaPendente: boolean = false
+
+console.log(tarefaConcluida)
+console.log(tarefaPendente)
+
+const concluido: boolean = false
+
+if (concluido) {
+    console.log('Tarefa foi concluída com sucesso!')
+} else {
+    console.log('Tarefa pendente!')
+}
